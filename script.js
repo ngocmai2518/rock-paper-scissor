0a11e01f7e74fac80f31ca160ae23e4a099f26ad
jadie <142028877+ngocmai2518@users.noreply.github.com>
2023-12-27 03:57:24 +0700

    fix bug caseWinner only show one array

--- script.js
+++ script.js
@@ -22,13 +22,29 @@ function getComputerChoice() {
 
 function playRound(playerSelection,computerSelection) {
   const array = [playerSelection,computerSelection];
-  const caseWinner = ["rock","scissor"] || ["paper","rock"] || ["scissor","paper"];
-  let result;
+  const caseWinner = [["rock","scissor"],["paper","rock"],["scissor","paper"]];
+  function caseWinnerIncludesarray(caseWinner,array) {
+    for (let i=0; i <= caseWinner.length - array.length; i++) {
+      if (Array.isArray(caseWinner[i]) && caseWinner[i].length === array.length) {
+        let match = true;
+        for (let j = 0; j < caseWinner.length; j++) {
+          if (caseWinner[i][j] !== array[j]) {
+              match = false;
+              break;
+          }
+        } 
+        if (match) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
 
   if (playerSelection === computerSelection) {
     result = "Draw, please play again"
   }
-  else if (JSON.stringify(array) === JSON.stringify(caseWinner)) {
+  else if (caseWinnerIncludesarray(caseWinner,array)) {
     result = "You win! " + playerSelection.charAt(0).toUpperCase() 
       + playerSelection.slice(1) + " beats " + computerSelection + "!"
   }
@@ -39,7 +55,7 @@ function playRound(playerSelection,computerSelection) {
   return result;
 }
 
-let playerSelection = getPlayerChoice("scissor");
+let playerSelection = getPlayerChoice("paper");
 console.log(playerSelection);
 
 let computerSelection = getComputerChoice();
